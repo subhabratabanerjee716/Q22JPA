1d462135d5840f2244ae8a9ff324908bad94f412
Subhabrata Banerjee <63787106+subhabratabanerjee716@users.noreply.github.com>
2023-08-08 15:55:09 +0530

    Create Q22Application.java

---
+++ Q22Application.java
@@ -0,0 +1,13 @@
+package com.Subha.ineuron.q22.Q22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q22Application.class, args);
+	}
+
+}
